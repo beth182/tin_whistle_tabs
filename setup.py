86223d8f10674d93fcb6883c7f4d5371aa95cc65
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-05-14 13:29:59 +0100

    create setup and install package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tin_whistle_tabs',
+    url='https://github.com/beth182/tin_whistle_tabs',
+    author='Beth Saunders',
+    author_email='bethany.saunders@pgr.reading.ac.uk',
+    packages=[],
+    install_requires=[
+        'matplotlib'
+    ],
+    version='0.1',
+    license='MIT',
+    description='Create simple tin whistle tabs',
+    long_description='Create simple tin whistle tabs using python'
+)
